631c9c03f2d5337491381d4c64263ffc5aa2410c
Fillipek <mar.lim.420@onet.pl>
2021-01-07 10:36:03 +0100

    add browsing for the rest of main tables

--- controllers/db-controller.js
+++ controllers/db-controller.js
@@ -8,15 +8,29 @@ class DbController {
             rasa AS "Rasa", \
             data_urodzenia AS "Data urodzenia" \
             FROM schronisko.zwierzeta')
-        // res.send(result);
-        res.render("browse", {data : result});
+        res.render("browse", {data : result, title : "Zwierzęta"});
     }
     selectPersonel = async (req, res) => {
         let result = await pool.query('SELECT imie AS "Imię", nazwisko AS "Nazwisko",\
-         pensja/100. AS "Pensja", pesel AS "PESEL", nazwa AS "Stanowisko"\
+         CAST(pensja/100. AS NUMERIC(10,2)) AS "Pensja", pesel AS "PESEL", nazwa AS "Stanowisko"\
           FROM schronisko.personel NATURAL JOIN schronisko.grupy_zawodowe')
-        // res.send(result);
-        res.render("browse", {data : result});
+        res.render("browse", {data : result,  title : "Personel"});
+    }
+    selectWpisy = async (req, res) => {
+        let result = await pool.query('SELECT * FROM schronisko.zwierzeta_info')
+        res.render("browse", {data : result, title : "Zwierzęta Info"});
+    }
+    selectBoksy = async (req, res) => {
+        let result = await pool.query('SELECT * FROM schronisko.boksy')
+        res.render("browse", {data : result, title : "Boksy"});
+    }
+    selectPawilony = async (req, res) => {
+        let result = await pool.query('SELECT * FROM schronisko.pawilony')
+        res.render("browse", {data : result, title : "Pawilony"});
+    }
+    selectMagazyn = async (req, res) => {
+        let result = await pool.query('SELECT * FROM schronisko.magazyn')
+        res.render("browse", {data : result, title : "Magazyn"});
     }
 }
 
--- routers/db-router.js
+++ routers/db-router.js
@@ -4,4 +4,8 @@ const controller = require('../controllers/db-controller');
 const router = Router();
 router.get('/zwierzeta', controller.selectZwierzeta);
 router.get('/personel', controller.selectPersonel);
+router.get('/zwierzeta_info', controller.selectWpisy);
+router.get('/pawilony', controller.selectPawilony);
+router.get('/boksy', controller.selectBoksy);
+router.get('/magazyn', controller.selectMagazyn);
 module.exports = router;
